d6554801c608acad6bc67ed9cf0d3fa6b7e975ff
Pollito400 <dylanramirezdeveloper@outlook.com>
2026-01-09 07:04:53 -0600

    Update video_stats.py

--- Youtube/video_stats.py
+++ Youtube/video_stats.py
@@ -6,9 +6,26 @@ API_KEY = ""
 CHANNEL_HANDLE = "jujalag"
 url = f'https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}'
 
-response = requests.get(url)
-print(response)
+def get_json_channel(url: str):
+    try:
+        response = requests.get(url, timeout=15)
+        response.raise_for_status()
+        
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        raise e
 
-data = response.json()
-print(data)
-print(json.dumps(data, indent=4))
+def get_play_list_id(url: str):
+    try:
+        data = get_json_channel(url)
+
+        chaneel_items = data["items"][0]
+        channel_playlist_id = chaneel_items["contentDetails"]["relatedPlaylists"]
+        
+        return channel_playlist_id
+    except requests.exceptions.RequestException as e:
+        raise e
+
+if __name__ == "__main__":
+    playlists = get_play_list_id(url)
+    print(playlists)
