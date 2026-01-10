68100941b12e2ab454ed65942f7b4b93a5f43eb8
Pollito400 <dylanramirezdeveloper@outlook.com>
2026-01-10 13:26:10 -0600

    Update video_stats.py

--- Youtube/video_stats.py
+++ Youtube/video_stats.py
@@ -19,39 +19,33 @@ def get_json(url: str):
         raise e
 
 def get_play_list_id(channel_handle=CHANNEL_HANDLE, api_key=API_KEY):
-    try:
-        url = f'https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&id={channel_handle}&key={api_key}'
-        data = get_json(url)
+    url = f'https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={channel_handle}&key={api_key}'
+    data = get_json(url)
+    channel_items = data["items"][0]
+    return channel_items["contentDetails"]["relatedPlaylists"]["uploads"]
 
-        chaneel_items = data["items"][0]
-        channel_playlist_id = chaneel_items["contentDetails"]["relatedPlaylists"]['uploads']
-        
-        return channel_playlist_id
-    except requests.exceptions.RequestException as e:
-        raise e
 
 def get_videos_play_ids(maxResults=maxResults, api_key=API_KEY):
-    channel_playlist_id = get_play_list_id()
-    url = f'https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={maxResults}&playlistId={channel_playlist_id}&key={api_key}'
+    channel_playlist_id = get_play_list_id(api_key=api_key)
+
     video_ids = []
     pageToken = None
-    
-    try:
-        while True:
-            if pageToken:
-                base_url += f"pageToken={pageToken}"
-            data = get_json(url)
-            for item in data.get('items', []):
-                video_id = item["items"][0]["contentDetails"]
-                video_ids .append(video_id)
-            pageToken = data.get('nextPageToken')
-            
-            if not pageToken:
-                break
-        return video_ids
 
-    except requests.exceptions.RequestException as e:
-        raise e
+    while True:
+        url = f'https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={maxResults}&playlistId={channel_playlist_id}&key={api_key}'
+        if pageToken:
+            url += f"&pageToken={pageToken}"
+
+        data = get_json(url)
+
+        for item in data.get("items", []):
+            video_ids.append(item["contentDetails"]["videoId"])
+
+        pageToken = data.get("nextPageToken")
+        if not pageToken:
+            break
+
+    return video_ids
 
 
 if __name__ == "__main__":
