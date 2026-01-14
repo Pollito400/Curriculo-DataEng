c803c53b32e0390218df12f752679d7f1d68e456
Pollito400 <dylanramirezdeveloper@outlook.com>
2026-01-14 12:43:23 -0600

    Update Api_Requests_Spotify.py

--- Spotify/Api_Requests_Spotify.py
+++ Spotify/Api_Requests_Spotify.py
@@ -19,7 +19,9 @@ def get_user(user: str, acess_token: str):
     try:
         url = f"https://api.spotify.com/v1/users/{user}"
         headers = {"Authorization": f"Bearer {acess_token}"}
-        return get_json(url, headers=headers)
+        user_data = get_json(url, headers=headers)
+        return user_data
+    
     except requests.exceptions.RequestException as e:
         raise e
 
@@ -28,14 +30,32 @@ def get_artist(id_artist: str, access_token: str):
     try:
         url = f"https://api.spotify.com/v1/artists/{id_artist}"
         headers = {"Authorization": f"Bearer {access_token}"}
-
         artist_data = get_json(url, headers=headers)
-
         return artist_data
 
     except (requests.exceptions.RequestException, KeyError, ValueError, TypeError, IndexError) as e:
         raise e
 
+def get_artis_album(id_artist: str, acess_token: str):
+    try:
+        url = f'https://api.spotify.com/v1/artists/{id_artist}/albums?include_groups=album%2Csingle%2Cappears_on%2Ccompilation&limit=50'
+        headers = {"Authorization": f"Bearer {acess_token}"}
+        artist_album_data = get_json(url, headers=headers)
+        return artist_album_data
+    
+    except (requests.exceptions.RequestException, KeyError, ValueError, TypeError, IndexError) as e:
+        raise e
+ 
+def get_album_tracks(album_id:str, acess_token: str):
+    try:
+        url = f'https://api.spotify.com/v1/albums/{album_id}/tracks?limit=50'
+        headers = {"Authorization": f"Bearer {acess_token}"}
+        album_data = get_json(url, headers=headers)
+        return album_data
+    except (requests.exceptions.RequestException, KeyError, ValueError, TypeError, IndexError) as e:
+        raise e
+ 
+    
 if __name__ == "__main__":
     if not ACCESS_TOKEN:
         raise ValueError("Falta SPOTIFY_ACCESS_TOKEN en .env")
